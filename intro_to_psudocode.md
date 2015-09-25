6f3fea2bca5c6d04b872cc832e8f974776194308
Obtuse Autodidact <theobtuseautodidact@gmail.com>
2015-09-24 18:59:59 -0600

    refactoring calculator

---
+++ intro_to_psudocode.md
@@ -0,0 +1,21 @@
+For example, write out pseudo-code (both casual and formal) that does the following:
+
+1. a method that returns the sum of two integers
+2. a method that takes an array of strings, and returns a string that is all those strings concatenated together
+3. a method that takes an array of integers, and returns a new array with every other element
+No need to code here, just practice writing logic out in English.
+
+1.
+GET ints A and B
+return A + B
+
+2.
+GET array_of_strings
+SET a variable to an empty string
+iterate through array concat'ing each string item to the variable string
+return variable
+
+3.
+GET array_of_ints
+iterate through array_of_ints, collecting into a new array, odd(or even) indexed array items
+return new array
--- lesson2/calculator.rb
+++ lesson2/calculator.rb
@@ -35,23 +35,115 @@
 # ask the user for an operation to perform
 # perform the operation on the two numbers
 # output the result
-
-puts "Welcome to the calulator!"
-puts "what's the first number?"
-number1 = gets.chomp.to_i
-puts "what the second number?"
-number2 = gets.chomp.to_i
-puts "what operation would ou like to perform? 1) add 2) subtract 3) multiply 4) divide"
-operator = gets.chomp
-
-if operator == '1'
-  result = number1 + number2
-elsif operator == '2'
-  result = number1 - number2
-elsif operator == '3'
-  result = number1 * number2
-elsif operator == '4'
-  result = number1.to_f / number2.to_f
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-puts "result is #{result}"
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    "Adding"
+  when '2'
+    "Subtracting"
+  when '3'
+    "Multipying"
+  when '4'
+    "Dividing"
+  end
+end
+
+prompt("Welcome to the calulator! Enter your name: ")
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt("make sure to use a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do #main loop
+
+  number1 = ''
+  loop do
+  prompt("what's the first number?")
+  number1 = gets.chomp.to_i
+
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number.")
+  end
+  end
+
+  number2 = ''
+  loop do
+  prompt("what the second number?")
+  number2 = gets.chomp.to_i
+
+  if valid_number?(number2)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number.")
+  end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+  operator = ''
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, or 4")
+    end
+  end
+
+  # if operator == '1'
+  #   result = number1 + number2
+  # elsif operator == '2'
+  #   result = number1 - number2
+  # elsif operator == '3'
+  #   result = number1 * number2
+  # elsif operator == '4'
+  #   result = number1.to_f / number2.to_f
+  # end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+  when '1'
+  number1 + number2
+  when '2'
+  number1 - number2
+  when '3'
+  number1 * number2
+  when '4'
+  number1.to_f / number2.to_f
+  end
+
+  prompt("result is #{result}")
+
+  prompt("Do you want to perform another calculation?(Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+
+end
+
+prompt("Thank you for using the calculator. Good bye")
